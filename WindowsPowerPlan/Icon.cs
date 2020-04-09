aa42fed428399ce04bc1f4ff43391c95dce17584
windr00 <imrwz@icloud.com>
2020-04-09 21:45:13 +0800

    first commit

---
+++ WindowsPowerPlan/Icon.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsPowerPlan
+{
+    partial class Icon : IDisposable
+    {
+        private NotifyIcon icon = new NotifyIcon();
+
+        private ContextMenuStrip menu = new ContextMenuStrip();
+
+        private void Initialize()
+        {
+
+            menu.Opening += Menu_Opening;
+            icon.Icon = TrayIcon;
+            icon.Text = "Windows Power Plan Switcher";
+            icon.ContextMenuStrip = menu;
+            icon.Visible = true;
+            Menu_Opening(this, new CancelEventArgs(false)); //pseudo first click
+        }
+
+        string[] runProcess(string cmd, params string[] args)
+        {
+            string arg = "";
+            foreach (var a in args)
+            {
+                arg += a + " ";
+            }
+            var proc = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = cmd,
+                    Arguments = arg,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    CreateNoWindow = true
+                }
+            };
+            proc.Start();
+            var lines = new List<string>();
+            while (!proc.StandardOutput.EndOfStream)
+            {
+                lines.Add(proc.StandardOutput.ReadLine());
+            }
+            return lines.ToArray();
+        }
+        
+
+
+        private void Menu_Opening(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            menu.Items.Clear();
+            var lines = runProcess("powercfg.exe", "-l");
+            foreach (var line in lines)
+            {
+                if (line.Contains("GUID"))
+                {
+                    var foundGUID = line.Split(' ')[2];
+                    var menuItem = new ToolStripMenuItem(line, null, (o, args) => { runProcess("powercfg.exe", "-s", foundGUID); });
+                    if (line.Contains("*"))
+                    {
+                        menuItem.Checked = true;
+                        menuItem.CheckState = CheckState.Checked;
+                    }
+                    else
+                    {
+                        menuItem.Checked = false;
+                        menuItem.CheckState = CheckState.Unchecked;
+                    }
+                    menu.Items.Add(menuItem);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            icon.Visible = false;
+            icon?.Dispose();
+        }
+    }
+}
---
+++ WindowsPowerPlan/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WindowsPowerPlan
+{
+    static class Program
+    {
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern bool SetProcessDPIAware();
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            SetProcessDPIAware();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            using (new Icon())
+            {
+                Application.Run();
+            }
+        }
+    }
+}
